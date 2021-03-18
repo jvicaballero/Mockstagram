412e7b9b2aeccf6fefdfb67831681a157e9c369f
Jasper Caballero <jvicaballero@gmail.com>
2021-03-18 00:22:33 -0400

    Completed Required Stories for Part 2: Bottom Navbar, shift to pressed fragment, added profile and home fragments to see posts.

---
+++ app/src/main/java/com/example/mockstagram/fragments/PostFragment.java
@@ -0,0 +1,80 @@
+package com.example.mockstagram.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.mockstagram.Post;
+import com.example.mockstagram.PostAdapter;
+import com.example.mockstagram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostFragment extends Fragment {
+
+    public static final String TAG = "PostFragment";
+    private RecyclerView rvPosts;
+    protected PostAdapter adapter;
+    protected List<Post> allPosts;
+    public PostFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        allPosts = new ArrayList<>();
+
+        rvPosts = view.findViewById(R.id.rvPosts);
+        adapter = new PostAdapter(getContext(), allPosts);
+
+        rvPosts.setAdapter(adapter);
+
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+    }
+
+    protected void queryPosts() {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.setLimit(20);
+        query.addDescendingOrder(Post.KEY_CREATED_KEY);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e != null){
+                    Log.e(TAG, "Issue with getting posts");
+                    return;
+                }
+
+                for(Post post : posts) {
+                    Log.i(TAG, "Post: " + post.getDescription() + ", username of user =" + post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
+    }
+}
